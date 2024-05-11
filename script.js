9cf9686199ea796fcf375cf68f4eb36dd7dedd61
Jonathan Copeland <jonathan.copeland@hotmail.co.uk>
2024-05-11 10:19:32 +0100

    Completed js functionality

--- script.js
+++ script.js
@@ -17,4 +17,24 @@ let morseAlphaNumArr = morseNumArr.concat(morseAlphaArr);
 
 console.log(morseAlphaNumArr);
 
+let userInputStr = prompt("Enter Text:");
+
+let userInputArr = userInputStr.toUpperCase().trim().split("");
+
+let outputMorseArr = [];
+
+for(let i = 0; i < userInputArr.length; i++){
+    for(let j = 0; j < 36; j++){
+        if(userInputArr[i] === alphaNumArr[j]){
+            outputMorseArr.push(morseAlphaNumArr[j]);
+        }
+    }
+}
+
+let outputMorseStr = outputMorseArr.join(" ");
+
+alert(outputMorseStr);
+
+
+
 
