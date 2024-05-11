9a09412737c06210a20de3ac0c600d9d03b31e9e
Jonathan Copeland <jonathan.copeland@hotmail.co.uk>
2024-05-11 09:57:11 +0100

    concat arrays

--- script.js
+++ script.js
@@ -2,7 +2,19 @@ let alphaNumStr = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 
 let alphaNumArr = alphaNumStr.split("");
 
+console.log(alphaNumArr);
+
 let morseNumArr = ["-----", ".----", "..---", "...--", "....-", ".....", "-....", "--...", "---..", "----."];
+
+console.log(morseNumArr);
 
 let morseAlphaArr = [".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---", "-.-", ".-..", "--", "-.", "---", ".--.",
 "--.-", ".-.", "...", "-", "..-", "...-", ".--", "-..-", "-.--", "--.."];
+
+console.log(morseAlphaArr);
+
+let morseAlphaNumArr = morseNumArr.concat(morseAlphaArr);
+
+console.log(morseAlphaNumArr);
+
+
